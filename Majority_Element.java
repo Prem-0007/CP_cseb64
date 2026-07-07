3e194b42200226d6ae74dd838eb30b59519a0ac0
Prem <premkumarballa777@gmail.com>
2026-07-07 10:17:13 +0530

    Fix class declaration formatting in Majority_Element.java

--- Majority_Element.java
+++ Majority_Element.java
@@ -4,8 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
-    
+public class Solution { 
     public static void main(String[] args) {
      Scanner sc=new Scanner(System.in);
      int n=sc.nextInt();
